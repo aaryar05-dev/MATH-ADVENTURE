94abb9cd3847b4a2fda0527c5a9c4879b6c9545b
aaryar05-dev <aaronyarleque05@gmail.com>
2026-04-12 20:06:50 -0500

    Primera Carga

---
+++ src/main/java/com/mycompany/math/adventure/MATHADVENTURE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.math.adventure;
+
+/**
+ *
+ * @author pikar
+ */
+public class MATHADVENTURE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
